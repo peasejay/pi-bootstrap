8917b4c6bf00306dceca1d76c5a6dcf363a0bb6c
Jeff Pease <jpease@peasej.com>
2016-07-25 01:00:18 +0000

    initial commit. setting wifi settings and system locale

---
+++ bootstrap.sh
@@ -0,0 +1,62 @@
+if [ $(id -u) -ne 0 ]; then
+  printf "Script must be run as root. Exiting..."
+  echo
+  echo
+  exit 1
+fi
+
+
+WIFI_CONFIG=/etc/wpa_supplicant/wpa_supplicant.conf
+LOCALE_CONFIG=/etc/locale.gen
+
+. ./config.dat
+if [ -e ./localconfig.dat ]; then
+  . ./localconfig.dat
+fi
+
+
+if grep -q "$WIFI_SSID" $WIFI_CONFIG ; then
+  echo "Found $WIFI_SSID... skipping wifi setup"
+  echo
+else
+  echo "Could not find network $WIFI_SSID... adding it"
+  echo
+  cat <<WIFI >> $WIFI_CONFIG 
+
+network={
+   ssid="$WIFI_SSID"
+   psk="$WIFI_PSK"
+}
+WIFI
+  
+  echo "Restarting wifi..."
+  sleep 5
+  ifdown wlan0
+  sleep 5
+  ifup wlan0
+  sleep 5
+  echo "Giving system a bit more time to resolve DNS..."
+  sleep 20
+  echo
+fi
+
+echo "Current wifi status:"
+iwconfig
+echo
+
+echo "Updating system software components..."
+apt-get update
+apt-get upgrade -y
+echo
+
+echo "Installing essential software components..."
+apt-get install -y vim git
+
+echo "Updating system locales..."
+# deactivate pi defaults 
+sed -i "s/^\(en_GB.UTF-8.*\)/# \1/" $LOCALE_CONFIG
+# activate preferred defaults
+sed -i "s/^# \($LOCALE.*\)/\1/" $LOCALE_CONFIG
+locale-gen
+
+
